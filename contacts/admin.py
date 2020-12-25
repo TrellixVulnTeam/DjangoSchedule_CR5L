b3af975e217adf9c9bf9f389b34e04f7cb4b9f9e
wictor-parmenis <wictortec@gmail.com>
2020-12-25 12:38:51 -0300

    Edit model of database and model of admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categoria, Contact
 
 # Register your models here.
+# python manage.py createsuperuser
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
+
+admin.site.register(Categoria)
+admin.site.register(Contact, ContactAdmin)
+
+
--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+'''
+    CONTATOS
+    id: INT (automático)
+    nome: STR * (obrigatório)
+    sobrenome: STR (opcional)
+    telefone: STR * (obrigatório)
+    email: STR (opcional)
+    data_criacao: DATETIME (automático)
+    descricao: texto
+    categoria: CATEGORIA (outro model)
+
+     CATEGORIA
+     id: INT
+     nome: STR * (obrigatório)
+    '''
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=55)
+
+    def __str__(self):
+        return self.nome
+
+class Contact(models.Model):
+    nome = models.CharField(max_length=55)
+    sobrenome = models.CharField(max_length=55, blank=True) # optional field.
+    telefone = models.CharField(max_length=55)
+    email = models.CharField(max_length=55, blank=True) # optional field.
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True) # optional field
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
+
+
+'''
+NEXT STEPS:
+python manage.py makemigrations   (FAZER AS MIGRAÇÕES)
+python manage.py migrate          (EXECUTAR ALTERAÇÕES)
+'''
+
+'''
+IMPORTANT:
+Após qualquer alteração deve-se utilizar os 2 comandos acima para
+validar as alterações no migrations.
+'''
+
+
+

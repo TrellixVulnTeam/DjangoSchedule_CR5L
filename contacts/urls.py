b35a1a0d013c8a5dee1bbc13d45ebd2a82145082
wictor-parmenis <wictortec@gmail.com>
2020-12-27 09:42:00 -0300

    update urls contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:contact_id>', views.view_contact, name='view_contact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'contacts/index.html')
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/index.html', {
+        'contacts' : contacts
+    })
+
+
+def view_contact(request, contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    return render(request, 'contacts/view_contact.html', {
+        'contact' : contact
+    })

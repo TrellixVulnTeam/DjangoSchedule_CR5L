e349b4163fee25351446aa489c4f79d5354d81a5
wictor-parmenis <wictortec@gmail.com>
2021-01-02 11:21:54 -0300

    Init login, register, dashboard and logout pages.

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path, include
+
+urlpatterns = [
+    path('', views.login, name='index_login'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    ]
